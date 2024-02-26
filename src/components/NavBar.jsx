443989f87761015cb9ad20dc241de43ff17e070f
Lucie Yarish <lucieyarish@hotmail.com>
2024-02-26 06:35:00 +0100

    Create and style navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav className="nav-bar">
+      <img
+        src="/assets/airbnb-logo.png"
+        alt="Red-colored Airbnb logo"
+        className="navbar--logo"
+      />
+    </nav>
+  );
+};
+
+export default NavBar;
