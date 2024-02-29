f40db45c16d4d6b5c51b8a87922aedb9eee265cb
Lucie Yarish <lucieyarish@hotmail.com>
2024-02-29 07:27:05 +0100

    Allocate all photo related data in a separate data file

---
+++ src/data/data.js
@@ -0,0 +1,48 @@
+export default [
+  {
+    id: 1,
+    title: 'Life Lessons with Katie Zaferes',
+    description:
+      'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
+    price: 136,
+    coverImg: '/assets/katie-zaferes.png',
+    altText: 'Katie Zaferes smiling into a camera with her swimsuit on',
+    stats: {
+      rating: 5.0,
+      reviewCount: 6,
+    },
+    location: 'Online',
+    openSpots: 0,
+  },
+  {
+    id: 2,
+    title: 'Learn Wedding Photography',
+    description:
+      "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+    price: 125,
+    coverImg: '/assets/wedding-photography.png',
+    altText:
+      'Bride in a white wedding dress, standing in a hallway in the center of the photo and looking into the camera',
+    stats: {
+      rating: 5.0,
+      reviewCount: 30,
+    },
+    location: 'Online',
+    openSpots: 27,
+  },
+  {
+    id: 3,
+    title: 'Group Mountain Biking',
+    description:
+      'Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)',
+    price: 50,
+    coverImg: '/assets/mountain-bike.png',
+    altText: 'Mountain bike standing on a mountain peak in a bright, sunny day',
+    stats: {
+      rating: 4.8,
+      reviewCount: 2,
+    },
+    location: 'Norway',
+    openSpots: 3,
+  },
+];
