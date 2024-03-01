82ae1a0f06f664a5b2738438aab678206bf8cf08
Lucie Yarish <lucieyarish@hotmail.com>
2024-03-01 06:39:48 +0100

    Pass object as props

--- src/layout/Card.jsx
+++ src/layout/Card.jsx
@@ -2,15 +2,19 @@ import './Card.css';
 
 const Card = (props) => {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = 'SOLD OUT';
-  } else if (props.location === 'Online') {
+  } else if (props.item.location === 'Online') {
     badgeText = 'ONLINE';
   }
 
   return (
     <div className="card">
-      <img className="card--image" src={props.coverImg} alt={props.altText} />
+      <img
+        className="card--image"
+        src={props.item.coverImg}
+        alt={props.item.altText}
+      />
       {badgeText && <div className="card--badge">{badgeText}</div>}
       <div className="card--rating-section">
         <img
@@ -18,17 +22,17 @@ const Card = (props) => {
           src="/assets/star.png"
           alt="Icon of a red star"
         />
-        <p className="card--text">{props.rating}</p>
-        <p className="card--text-grey">({props.reviewCount})</p>
+        <p className="card--text">{props.item.stats.rating}</p>
+        <p className="card--text-grey">({props.item.stats.reviewCount})</p>
         <p className="card--text-grey">•</p>
-        <p className="card--text-grey">{props.location}</p>
+        <p className="card--text-grey">{props.item.location}</p>
       </div>
       <div className="card--description">
-        <h2 className="card--text">{props.title}</h2>
+        <h2 className="card--text">{props.item.title}</h2>
       </div>
       <div className="card--price">
         <p className="card--text card--text-bold">
-          From ${props.price}{' '}
+          From ${props.item.price}{' '}
           <span className="card--text-regular">/ person</span>
         </p>
       </div>
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,19 +4,7 @@ import data from '../data/data';
 
 const Main = () => {
   const cards = data.map((item) => {
-    return (
-      <Card
-        key={item.id}
-        coverImg={item.coverImg}
-        altText={item.altText}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
-      />
-    );
+    return <Card key={item.id} item={item} />;
   });
   return (
     <main className="main--container">
